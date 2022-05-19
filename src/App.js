ab9489054a7bb6e72f991865176ab629cd295fc5
Maru <marianalopezeikilis>
2022-05-19 12:44:03 +0200

    React animales

--- src/App.js
+++ src/App.js
@@ -3,22 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div className="menu">
+      <p>Menu</p>
+      <a class="colorLink" >Aves</a>
+      <a class="colorLink" >Reptiles</a>
+      <a class="colorLink">Mamíferos</a>
+      <a class="colorLink" >Anfibios</a>
+      <a class="colorLink">Peces</a>
+    </div><br /><div className="divGrande">
+        <h1>Mamíferos</h1>
+        <div className="card">
+          <p>Osos</p>
+          <img src="./imagenes/oso.jpg" />
+        </div>
+        <div className="card">
+          <p>Tigre</p>
+          <img src="./imagenes/tigre.jpg" />
+        </div>
+        <div className="card">
+          <p>Lobo</p>
+          <img src="./imagenes/lobo.jpg" />
+        </div>
+        <br />
+        <div className="card">
+          <p>Leon</p>
+          <img src="./imagenes/leon.jpg" />
+        </div>
+        <div className="card">
+          <p>Koala</p>
+          <img src="./imagenes/koala.jpg" />
+        </div>
+        <div className="card">
+          <p>Gato</p>
+          <img src="./imagenes/Gato.jpg" />
+        </div>
+      </div><br /><br /></>
+    
   );
 }
 
